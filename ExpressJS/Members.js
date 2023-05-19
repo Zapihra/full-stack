88223ee2592853fe26b39487dbe544cab3a0e145
Zapihra <iida.vaaja@student.lut.fi>
2023-05-19 19:40:38 +0300

    start of expressjs

---
+++ ExpressJS/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'john doe',
+        email: 'john@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'bob williams',
+        email: 'bob@email.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'shannon jackson',
+        email: 'shannon@email.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
---
+++ ExpressJS/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+const members = require('./Members');
+
+const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${
+        req.originalUrl}:${moment().format()}`);
+    next();
+}
+
+//init middleware
+app.use(logger)
+
+//gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
