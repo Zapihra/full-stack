56a069427ecfec293eb946321120b6c7ef594377
Zapihra <iida.vaaja@student.lut.fi>
2023-05-20 18:15:48 +0300

    added a way to add members, added uuid and added members api

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
-const members = require('./Members');
 
 const app = express();
 
@@ -10,17 +9,15 @@ const app = express();
 //init middleware
 //app.use(logger)
 
-//gets all members
-app.get('/api/members', (req, res) => res.json(members));
-
-//gets single member
-app.get('/api/members/:id', (req, res) =>{
-    //res.send(req.params.id);
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-});
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//member api routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
